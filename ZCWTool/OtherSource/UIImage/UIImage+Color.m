ffa4f6a1aa109563a820891186bd248caff74ed0
zhaochangwu <changwuzhao@cienet.com.cn>
2017-05-17 16:56:03 +0800

    Add category method

--- ZCWTool/OtherSource/UIImage/UIImage+Color.m
+++ ZCWTool/OtherSource/UIImage/UIImage+Color.m
@@ -11,20 +11,20 @@
 @implementation UIImage (Color)
 
 /** 根据颜色生成纯色图片 */
-//+ (UIImage *)imageWithColor:(UIColor *)color
-//{
-//    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
-//    UIGraphicsBeginImageContext(rect.size);
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//    
-//    CGContextSetFillColorWithColor(context, [color CGColor]);
-//    CGContextFillRect(context, rect);
-//    
-//    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-//    UIGraphicsEndImageContext();
-//    
-//    return image;
-//}
++ (UIImage *)imageWithColor:(UIColor *)color
+{
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
 
 /** 取图片某一像素的颜色 */
 - (UIColor *)colorAtPixel:(CGPoint)point
@@ -87,6 +87,18 @@
     CGImageRelease(contextRef);
     
     return grayImage;
+}
+
+- (UIImage *)imageWithTintColor:(UIColor *)tintColor {
+	//We want to keep alpha, set opaque to NO; Use 0.0f for scale to use the scale factor of the device’s main screen.
+	UIGraphicsBeginImageContextWithOptions(self.size, NO, self.scale);
+	[tintColor setFill];
+	CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
+	UIRectFill(bounds);
+	[self drawInRect:bounds blendMode:kCGBlendModeDestinationIn alpha:1.0f];
+	UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	return tintedImage;
 }
 
 @end
